6b2b8f3e18312cc208e1c1ec6771698f3826f110
Jakub <jakubkrys@gmail.com>
2021-08-23 20:25:08 +0200

    #2 zakomentowanie pobierania po ID

--- src/main/java/UniversityExample.java
+++ src/main/java/UniversityExample.java
@@ -10,8 +10,8 @@ public class UniversityExample {
         EntityManager entityManager = emf.createEntityManager();
 
         // pobieranie po ID
-        Student student = entityManager.find(Student.class, 300);
-        System.out.println(student);
+//        Student student = entityManager.find(Student.class, 300);
+//        System.out.println(student);
 
         // zapis do bazy danych
 //        Student student1 = new Student("Adrian", "Spejson", "Puchacki", "adrian.puchacki@student.edu.pl", 222333444, 7, 404009, 87564231);
