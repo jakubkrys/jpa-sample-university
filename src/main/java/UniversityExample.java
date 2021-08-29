0663523acb1bd369a25e9c37848e7455487e73bd
Jakub <jakubkrys@gmail.com>
2021-08-29 13:35:03 +0200

    #2 przygotowanie menu pod aplikację

--- src/main/java/UniversityExample.java
+++ src/main/java/UniversityExample.java
@@ -1,6 +1,7 @@
 import Entities.Student;
 
 import javax.persistence.*;
+import java.util.*;
 
 public class UniversityExample {
 
@@ -8,19 +9,35 @@ public class UniversityExample {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("university");
         EntityManager entityManager = emf.createEntityManager();
+        Scanner scanner = new Scanner(System.in);
+        int option;
 
-        // pobieranie po ID
-//        Student student = entityManager.find(Student.class, 300);
-//        System.out.println(student);
+        do {
+            System.out.println("1 - save");
+            System.out.println("2 - get by ID");
+            System.out.println("0 - close app");
+            option = scanner.nextInt();
+            scanner.nextLine();
 
-        // zapis do bazy danych
-//        Student student1 = new Student("Adrian", "Spejson", "Puchacki", "adrian.puchacki@student.edu.pl", 222333444, 7, 404009, 87564231);
-//        try {
-//            entityManager.getTransaction().begin();
-//            entityManager.persist(student1);
-//            entityManager.getTransaction().commit();
-//        } catch (Exception e) {
-//            entityManager.getTransaction().rollback();
-//        }
+            switch (option) {
+                case 1:
+                    //zapis do bazy danych
+                    Student student1 = new Student("Adrian", "Spejson", "Puchacki", "adrian.puchacki@student.edu.pl", 222333444, 7, 404009, 87564231);
+                    try {
+                        entityManager.getTransaction().begin();
+                        entityManager.persist(student1);
+                        entityManager.getTransaction().commit();
+                    } catch (Exception e) {
+                        entityManager.getTransaction().rollback();
+                    }
+                    break;
+                case 2:
+                    // pobieranie po ID
+                    Student student = entityManager.find(Student.class, 300);
+                    System.out.println(student);
+                    break;
+            }
+        } while (option != 0);
+        scanner.close();
     }
 }
